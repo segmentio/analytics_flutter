c68b90d1dce7984bcf4c047f34b24295fe701825
Cody Garvin <emig647@gmail.com>
2021-08-30 16:15:17 -0700

    Initial library added

---
+++ lib/analytics.dart
@@ -0,0 +1,10 @@
+import 'package:sovran/store.dart';
+import 'timeline.dart';
+
+class Analytics {
+  late final Timeline timeline;
+
+  Analytics() {
+    this.timeline = new Timeline();
+  }
+}
---
+++ lib/timeline.dart
@@ -0,0 +1,7 @@
+class Timeline {
+  late final Map<String, String> plugins;
+
+  Timeline() {
+    this.plugins = new Map();
+  }
+}
