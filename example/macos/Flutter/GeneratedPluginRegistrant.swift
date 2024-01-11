05ecd4601b5bc9a287f5061b66fce6780f9fd7e1
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2024-01-11 09:50:37 -0800

    fix: refine the app installed flag check

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import analytics
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AnalyticsPlugin.register(with: registry.registrar(forPlugin: "AnalyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
