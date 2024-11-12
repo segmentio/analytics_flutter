1fa690201d2cd3f420a13080cd15ffe595dd407d
Rishabh Jain <risjain@twilio.com>
2024-11-12 11:36:51 +0530

    Updated flush_policy_test.dart

--- packages/core/test/flush_policies/flush_policy_test.dart
+++ packages/core/test/flush_policies/flush_policy_test.dart
@@ -22,12 +22,14 @@ void main() {
     test('start method sets startCalled to true', () {
       final policy = MockFlushPolicy();
       policy.start();
+      verify(policy.start()).called(1);
     });
 
     test('onEvent method sets onEventCalled to true', () {
       final policy = MockFlushPolicy();
       final event = TrackEvent("Test");
       policy.onEvent(event);
+      verify(policy.onEvent(event)).called(1);
     });
 
     test('reset method sets shouldFlush to false', () {
