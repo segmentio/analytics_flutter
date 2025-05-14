4905f18e3459301846d0fcce5f7373d6452d5b58
Neelkanth Kaushik <nkaushik@segment.com>
2025-05-14 22:35:18 +0530

    Patch for Github issue #138

--- packages/core/lib/analytics_web.dart
+++ packages/core/lib/analytics_web.dart
@@ -18,7 +18,7 @@ class AnalyticsPlatformImpl extends AnalyticsPlatform {
       NativeContext(
         app: NativeContextApp(
           name: web.window.navigator.appName,
-          version: web.window.navigator.appVersion,
+          version: getAppVersion(),
           namespace: web.window.navigator.appCodeName,
         ),
         userAgent: web.window.navigator.userAgent,
@@ -28,6 +28,16 @@ class AnalyticsPlatformImpl extends AnalyticsPlatform {
           width: web.window.screen.width,
         ),
       );
+      
+      /*
+          - Checks for <meta name="app-version" content="1.2.3"> in <root>/web/index.html 
+             and return the value inside 'content'
+          - Returns the browser version as fallback
+      */
+      String getAppVersion() {
+        final meta = web.document.querySelector('meta[name="app-version"]');
+        return meta?.getAttribute('content') ?? web.window.navigator.appVersion;
+      }
 }
 
 class AnalyticsWeb {
