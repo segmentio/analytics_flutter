517b20167604f48afd744dd592fa242470422606
Neelkanth Kaushik <nkaushik@segment.com>
2025-09-09 13:13:06 +0530

    Removed commented code

--- packages/core/lib/utils/store/unsupported.dart
+++ packages/core/lib/utils/store/unsupported.dart
@@ -29,6 +29,3 @@ class StoreImpl with Store {
     throw PlatformNotSupportedError();
   }
 }
-
-// StoreImpl createStoreImpl({bool storageJson = true}) => StoreImpl(storageJson: storageJson);
-// }
