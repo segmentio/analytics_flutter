f992e61af689c1f7c1459a91225b25cf0167637f
Sunita Prajapati <sunita.kuamari@daffodilsw.com>
2026-05-19 10:36:09 +0530

    feat:added support for SPM

---
+++ packages/core/macos/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "segment_analytics",
+    platforms: [
+        .macOS("10.14"),
+    ],
+    products: [
+        .library(name: "segment_analytics", targets: ["segment_analytics"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "segment_analytics",
+            dependencies: [],
+            path: "Classes"
+        ),
+    ]
+)
